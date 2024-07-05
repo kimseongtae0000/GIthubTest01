d7f34d70dfdae9e29103ccb1be3a761851b655eb
kimseongtae0000 <118097300+kimseongtae0000@users.noreply.github.com>
2024-07-05 16:46:08 +0900

    Add files via upload

---
+++ 00PApp04/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+
+namespace _00PApp04
+{
+    abstract class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("도형을 그리다.");
+        }
+    }
+    class Triangle : Shape
+    {
+        //Draw() Overeiding하세요. 삼각형을 그리다.
+        public override void Draw()
+        {
+            Console.WriteLine("도형을 그리다.");
+        }
+    }
+    class Rectangle : Shape
+    {
+        //Draw() Overeiding하세요. 사각형을 그리다.
+        public override void Draw()
+        {
+            Console.WriteLine("사각형을 그리다.");
+        }
+    }
+    class Circle : Shape
+    {
+        //Draw() Overeiding하세요. 원을 그리다.
+        public override void Draw()
+        {
+            Console.WriteLine("원을 그리다.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape shape = new Shape();
+            Triangle triangle = new Triangle();
+            Rectangle rectangle = new Rectangle();
+            Circle circle = new Circle();
+
+            triangle.Draw();
+            rectangle.Draw();
+            circle.Draw();
+        }
+    }
+}
---
+++ 00PApp05/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _00PApp05
+{
+    abstract class Mammal
+    {
+        public abstract void Eat(); //추상 메소드 abstract Methood
+    }
+    class Lion : Mammal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("사자가 먹습니다.");
+        }
+    }
+    class Tiger : Mammal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("호랑이가 먹습니다.");
+        }
+    }
+    class Dog : Mammal 
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("강아지가 먹습니다.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car car = new Car();  //Car 클래스를 abstract 클래스로 만들었기 때문.
+            Lion lion = new Lion();
+            Tiger tiger = new Tiger();
+            Dog dog = new Dog();
+            lion.Eat();
+            tiger.Eat();
+            dog.Eat();
+        }
+    }
+}
