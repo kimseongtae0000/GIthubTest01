9ec16e6368e2132f7a7b76c6187e603f4c3cc43d
kimseongtae0000 <118097300+kimseongtae0000@users.noreply.github.com>
2024-07-05 16:44:25 +0900

    Add files via upload

---
+++ InterfaceGudeCoding/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace InterfaceGudeCoding
+{
+    interface IWing
+    {
+        public void Fly();
+        public void Fly2()
+        {
+            Console.WriteLine("이렇게 코딩하세요");
+        }
+    }
+    class Unicon : IWing
+    {
+        public void Fly()
+        {
+            Console.WriteLine("나는 동작을 코딩한다~~~!!");
+        }
+        public void Fly2()
+        {
+            Console.WriteLine("이렇게 코딩하세요");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unicon u = new Unicon();
+            u.Fly2();
+        }
+    }
+}
---
+++ OOPApp06/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
+
+namespace OOPApp06
+{
+    class Car
+    {
+        public virtual void FuleSize()
+        {
+            Console.WriteLine("연료통이 없습니다.");
+        }
+        public void Price(int Price)
+        {
+            Console.WriteLine($"차량의 가격은 {Price}원 입니다.");
+        }
+        public void Brand(string Brand)
+        {
+            Console.WriteLine($"차량의 브랜드는 {Brand} 입니다.");
+        }
+    }
+    class Bus : Car
+    {
+        public override void FuleSize()
+        {
+            Console.WriteLine("연료통의 크기는 130L입니다.");
+        }
+    }
+    class Taxi : Car
+    {
+        public override void FuleSize()
+        {
+            Console.WriteLine("연료통의 크기는 50L입니다.");
+        }
+    }
+    class Truck : Car
+    {
+        public override void FuleSize()
+        {
+            Console.WriteLine("연료통의 크기는 150L입니다.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bus bus = new Bus();
+            Taxi taxi = new Taxi();
+            Truck truck = new Truck();
+            car.FuleSize();
+            bus.FuleSize();
+            taxi.FuleSize();
+            truck.FuleSize();
+
+            car.Price(100000000);
+            car.Brand("벤츠");
+
+            bus.Price(150000000);
+            bus.Brand("대우");
+
+            truck.Price(135000000);
+            truck.Brand("만");
+        }
+    }
+}
