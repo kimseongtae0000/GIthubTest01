7cd852d9663043a6366da46fe29eab052bf317d3
kimseongtae0000 <118097300+kimseongtae0000@users.noreply.github.com>
2024-07-05 16:56:40 +0900

    Add files via upload

---
+++ LoopApp02/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Quiz03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("﻿첫 번째 숫자를 입력하세요 : ");
+                int value1 = Int32.Parse(Console.ReadLine());
+
+                Console.Write("﻿연산자 (+, -, *, /)를 입력하세요 : ");
+                string op = Console.ReadLine();
+
+                Console.Write("﻿두 번째 숫자를 입력하세요 : ");
+                int value2 = Int32.Parse(Console.ReadLine());
+
+                if (op == "+")
+                {
+                    Console.WriteLine(value1 + value2);
+                }
+                else if (op == "-")
+                {
+                    Console.WriteLine(value1 - value2);
+                }
+                else if (op == "*")
+                {
+                    Console.WriteLine(value1 * value2);
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine((double)value1 / value2);
+                }
+                while (true)
+                {
+                    Console.Write("계산을 계속하시겠습니까? (y/n) : ");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "n")
+                    {
+                        Console.WriteLine("계산을 종료합니다.");
+                        break;
+                    }
+                    else
+                    {
+                        if (answer.ToLower() == "y")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("단어를 다시 확인해 주세요");
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SwitchApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ExamApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("﻿첫 번째 숫자를 입력하세요 : ");
+            int number1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("﻿연산자 (+, -, *, /)를 입력하세요 : ");
+            string modify = Console.ReadLine();
+
+            Console.Write("﻿두 번째 숫자를 입력하세요 : ");
+            int number2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(number1, modify, number2);
+        }
+    }
+}
