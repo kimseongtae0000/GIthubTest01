723957ff63f189861a636d2d9de6c4e41b9759cf
kimseongtae0000 <118097300+kimseongtae0000@users.noreply.github.com>
2024-07-05 16:41:42 +0900

    Add files via upload

---
+++ BubbleSort01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BubbleSort01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 4, 5, 7, 3, 2, 1, 9, 8 };
+            int temp;
+            for (int i = list.Length - 1; i > 0; i--)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    if (list[k] > list[k + 1])
+                    {
+                        temp = list[k];
+                        list[k] = list[k + 1];
+                        list[k + 1] = temp;
+                    }
+                }
+            }
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Property01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Property01
+{
+    class Person
+    {
+
+        public Person()
+        {
+            
+        }
+        public Person(double d)
+        {
+        }
+        public Person(string name)
+        {
+        }
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Bob";
+            Console.WriteLine("안녕하세요. " + p.Name + "씨");
+        }
+    }
+}
